48ab23c3a001a77be267c24067c8a97c994012f5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-19 21:36:07 +0700

    added kuota and jalur section

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -43,24 +43,85 @@
             <div class="terms-content">
                 <div class="regular-terms">
                     <h3 class="regular-terms-title">
-                        Syarat Umum
+                        A. Syarat Umum
                     </h3>
                     <ul class="regular-terms-list">
                         <li>Ijazah/Surat Keterangan Lulus/Kartu Peserta Ujian Sekolah <br>
                             <strong>*(Untuk CPD yang lulus 2022 jika belum ada ijazah, bisa dilengkapi ketika Ijazah sudah terbit.)</strong>
                         </li>
                         <li>Kartu Keluarga</li>
-                        <li>Akta Kelahiran</li>
+                        <li>Akta Kelahiran/Surat Kenal Lahir</li>
                         <li>KTP Orang Tua</li>
+                        <li>Rapor (Semester 1 s/d 5)</li>
+                        <li>Surat Pertanggung Jawaban Mutlak (SPTJM) Orang Tua</li>
                     </ul>
                 </div>
                 <div class="special-terms">
                     <h3 class="special-terms-title">
-                        Syarat Khusus
+                        B. Syarat Khusus
                     </h3>
+                    <ul class="special-terms-list">
+                        <li>Sertifikat Juara</li>
+                        <li>Kartu Kesejahteraan (KIP/KIS/KPS/KKS/PKH)/Terdaftar di DTKS</li>
+                        <li>Surat Keterangan Domisili</li>
+                        <li>Surat Tugas Orang Tua</li>
+                    </ul>
+
+                    <span class="keterangan">
+                        <span class="text-red-500">*</span> Persyaratan khusus disesuaikan dengan Jalur Pendaftaran yang dipilih. <br>
+                        <span class="text-red-500">**</span> Untuk melihat persyaratan berdasarkan Jalur Pendaftaran, silahkan klik tombol dibawah ini.
+                    </span>
+
+                    <div class="terms-btn-wrapper">
+                        <a href="{{ url('terms') }}" class="terms-btn btn btn-primary">Baca Persyaratan Pendaftaran</a>
+                    </div>
                 </div>
             </div>
         </div>
         {{-- End Persyaratan --}}
+
+        {{-- Jalur Pendaftaran --}}
+        <div class="jalur-section">
+            <h1 class="jalur-title">
+                Kuota dan Jalur Pendaftaran
+            </h1>
+            <div class="jalur-content">
+                <div class="kuota-wrapper">
+                    <h3 class="kuota-title">
+                        A. Kuota Pendaftaran
+                    </h3>
+                    <ul class="kuota-list">
+                        <li>Afirmasi (20%)</li>
+                        <li>Perpindahan Orang Tua/Anak Guru (5%)</li>
+                        <li>Prestasi (25%)</li>
+                        <li>Zonasi (50%)</li>
+                    </ul>
+                    <p class="kuota-desc">
+                        <span class="text-red-500">*</span> Informasi lebih lengkap tentang Kuota Pendaftaran, silahkan klik tombol dibawah ini.
+                    </p>
+                    <div class="kuota-btn-wrapper">
+                        <a href="{{ url('kuota') }}" class="btn btn-primary btn-kuota">Kuota Pendaftaran</a>
+                    </div>
+                </div>
+                <div class="jalur-wrapper">
+                    <h3 class="jalur-list-title">
+                        B. Jalur Pendaftaran
+                    </h3>
+                    <ul class="jalur-list">
+                        <li>Jalur Afirmasi</li>
+                        <li>Jalur Perpindahan Orang Tua/Anak Guru</li>
+                        <li>Jalur Prestasi</li>
+                        <li>Jalur Zonasi</li>
+                    </ul>
+                    <p class="jalur-desc">
+                        <span class="text-red-500">*</span> Informasi lebih lengkap tentang Jalur Pendaftaran, silahkan klik tombol dibawah ini.
+                    </p>
+                    <div class="jalur-btn-wrapper">
+                        <a href="{{ url('jalur') }}" class="btn btn-primary btn-jalur">Jalur Pendaftaran</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- End Jalur --}}
     </div>
 @endsection
