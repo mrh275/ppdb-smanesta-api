878909400bddbd0cdf3228f9c8aa530e6135c8cf
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-19 10:20:34 +0700

    added som properties for hero section

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2170,9 +2170,11 @@ hMenu.addEventListener("click", function () {
   if (!hMenu.classList.contains("show")) {
     this.classList.add("show");
     document.querySelector(".nav-menu").classList.add("show");
+    document.querySelector(".navbar").classList.add("show");
   } else {
     this.classList.remove("show");
     document.querySelector(".nav-menu").classList.remove("show");
+    document.querySelector(".navbar").classList.remove("show");
   }
 });
 
