b39f390663e3c31212b6f31ad8deb450b245a73a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-19 10:19:38 +0700

    added icon library from boxicons

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,6 +11,9 @@
 
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+
+    {{-- Font --}}
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
