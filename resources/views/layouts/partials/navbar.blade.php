5cec31d1c1313f2e8d5d004bbbb215b749df18dc
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-20 22:09:57 +0700

    changed some menu and link

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -4,35 +4,34 @@
         <a href="{{ url('') }}" class="brand-title">PPDB SMAN 1 RAWAMERTA</a>
     </div>
 
-    <ul class="nav-menu">
-        <li class="nav-item">
-            <a href="{{ url('') }}" class="nav-link">Beranda</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('kuota') }}" class="nav-link">Kuota Pendaftaran</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('jalur') }}" class="nav-link">Jalur Pendaftaran</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('terms') }}" class="nav-link">Persyaratan</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('data-pendaftar') }}" class="nav-link">Data Pendaftar</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('faq') }}" class="nav-link">FAQ</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('contact') }}" class="nav-link">Kontak</a>
-        </li>
-    </ul>
+    <div class="menu-wrapper">
+        <ul class="nav-menu">
+            <li class="nav-item">
+                <a href="{{ url('') }}" class="nav-link">Beranda</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('kuota-jalur') }}" class="nav-link">Kuota dan Jalur</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('terms') }}" class="nav-link">Persyaratan</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('data-pendaftar') }}" class="nav-link">Data Pendaftar</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('faq') }}" class="nav-link">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('contact') }}" class="nav-link">Kontak</a>
+            </li>
+        </ul>
 
-    <div class="toggle-darkMode" id="toggle-darkMode">
-        <span class="darkMode-btn flex w-4 overflow-x-hidden">
-            <i class="bx bxs-moon text-white"></i>
-            <i class="bx bxs-sun text-yellow-600"></i>
-        </span>
+        <div class="toggle-darkMode" id="toggle-darkMode">
+            <span class="darkMode-btn flex w-4 overflow-x-hidden">
+                <i class="bx bxs-moon text-white"></i>
+                <i class="bx bxs-sun text-yellow-600"></i>
+            </span>
+        </div>
     </div>
 
     <div class="toggle-menu">
