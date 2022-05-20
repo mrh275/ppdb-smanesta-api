426ad99e7659592218234b1e1d8a34d4776dca0d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-21 00:01:42 +0700

    created new model, migrations, factories and controllers

---
+++ app/Http/Controllers/DataKesejahteraanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataKesejahteraan;
+use Illuminate\Http\Request;
+
+class DataKesejahteraanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DataKesejahteraan  $dataKesejahteraan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DataKesejahteraan $dataKesejahteraan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DataKesejahteraan  $dataKesejahteraan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DataKesejahteraan $dataKesejahteraan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DataKesejahteraan  $dataKesejahteraan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DataKesejahteraan $dataKesejahteraan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DataKesejahteraan  $dataKesejahteraan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DataKesejahteraan $dataKesejahteraan)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/DataPeriodikController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataPeriodik;
+use Illuminate\Http\Request;
+
+class DataPeriodikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DataPeriodik  $dataPeriodik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DataPeriodik $dataPeriodik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DataPeriodik  $dataPeriodik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DataPeriodik $dataPeriodik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DataPeriodik  $dataPeriodik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DataPeriodik $dataPeriodik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DataPeriodik  $dataPeriodik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DataPeriodik $dataPeriodik)
+    {
+        //
+    }
+}
