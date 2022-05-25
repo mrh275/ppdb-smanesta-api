40a1b5d04163758eba047850beb7ac6c8342f098
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-25 13:07:56 +0700

    created component for preview bukti pendaftaran

---
+++ resources/views/layouts/partials/forms/print-register.blade.php
@@ -0,0 +1,50 @@
+<div class="cetak-pendaftaran-wrapper">
+    <h3 class="cetak-pendaftaran-title">
+        Bukti Pendaftaran PPDB
+    </h3>
+
+    <div class="card-bukti-pendaftaran my-4">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body bg-white w-full rounded-lg">
+            <div class="cetak-header flex justify-start items-center py-6 px-8">
+                <img src="{{ asset('assets/img/LOGO-SMALL.png') }}" alt="Logo Sekolah" class="cetak-logo w-28 h-28">
+                <div class="cetak-brand mx-4">
+                    <p class="cetak-brand-title leading-3">
+                        <span class="text-lg block leading-6 font-semibold">
+                            PEMERINTAH DAERAH PROVINSI JAWA BARAT
+                        </span>
+                        <span class="text-lg block leading-6 font-semibold">
+                            DINAS PENDIDIKAN
+                        </span>
+                        <span class="text-lg block leading-6 font-semibold">
+                            CABANG DINAS PENDIDIKAN WILAYAH IV
+                        </span>
+                        <span class="text-xl block leading-6 font-semibold">
+                            SMA NEGERI 1 RAWAMERTA
+                        </span>
+                        <span class="text-xs block leading-6">
+                            Jl. Garunggung - Panyingkiran Kec. Rawamerta Kab. Karawang
+                        </span>
+                        <span class="text-xs block leading-3">
+                            Email : sman1rwt@gmail.com Website : https://sman1rawamerta.sch.id
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <hr class="border-b-2 border-double border-zinc-800 w-[95%] mx-auto py-[1px]">
+            <div class="cetak-body">
+                <h3>BUKTI PENDAFTARAN</h3>
+                <h3></h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- End Dropzone -->
+
+
+@push('specifyjs')
+@endpush
