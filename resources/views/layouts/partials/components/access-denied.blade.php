d59af7c3c6f8c3624de7737a58188a80f885a9ef
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-26 19:07:01 +0700

    created modal for access denied redirection

---
+++ resources/views/layouts/partials/components/access-denied.blade.php
@@ -0,0 +1,31 @@
+<div class="py-6 sm:py-8 lg:py-12" id="close-access-denied">
+    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+        <div class="md:h-80 flex flex-col sm:flex-row bg-gray-200 rounded-lg overflow-hidden relative">
+            <button type="button" class="close-popup-denied-btn absolute top-0 right-2 text-2xl text-zinc-400 hover:text-zinc-600">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+            <!-- image - start -->
+            <div class="w-full sm:w-1/2 lg:w-2/5 h-48 sm:h-auto order-first sm:order-none bg-gray-300">
+                <img src="{{ asset('assets/img/access-denied.svg') }}" loading="lazy" alt="Photo by Andras Vas" class="w-full h-full object-center" />
+            </div>
+            <!-- image - end -->
+
+            <!-- content - start -->
+            <div class="w-full sm:w-1/2 lg:w-3/5 flex flex-col p-4 sm:p-8">
+                <h2 class="text-gray-800 text-xl md:text-2xl lg:text-4xl font-bold mb-4">Eits... Gak bisa wlee!😛</h2>
+
+                <p class="max-w-md text-gray-600 mb-1">
+                    Untuk melakukan pendaftaran silahkan datang langsung ke sekolah kami.
+                </p>
+                <p class="max-w-md text-gray-600 mb-4">
+                    Kalo temen-temen gak tau lokasi sekolah kami, bisa klik tombol dibawah ini.
+                </p>
+
+                <div class="mt-auto">
+                    <a href="https://goo.gl/maps/m9zFx47x3Ym1fvSy9" target="_blank" class="btn btn-primary">Lokasi</a>
+                </div>
+            </div>
+            <!-- content - end -->
+        </div>
+    </div>
+</div>
