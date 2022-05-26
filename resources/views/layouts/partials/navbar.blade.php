8d462e7d4ecca6c8016d6ea89ab2dcf3c475e654
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-26 19:07:12 +0700

    added login menu

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -27,6 +27,9 @@
             <li class="nav-item">
                 <a href="#contact" class="nav-link">Kontak</a>
             </li>
+            <li class="nav-item">
+                <a href="javascript:void(0)" class="nav-link" id="login-btn">Login</a>
+            </li>
         </ul>
 
         <div class="toggle-darkMode" id="toggle-darkMode">
@@ -43,3 +46,31 @@
         <span class="burger"></span>
     </div>
 </nav>
+
+<div class="login-form fixed z-50 w-full top-0 transition-all ease-in-out duration-300 hidden" id="login-form">
+    <div class="bg-zinc-700 bg-opacity-50 py-6 h-screen flex justify-center items-center">
+        <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+            <form class="max-w-lg bg-white border rounded-lg mx-auto overflow-hidden">
+                <div class="flex justify-center items-center bg-gray-100 p-4 relative">
+                    <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center">Login PPDB</h2>
+                    <button type="button" id="close-login-btn" class="close-login-form absolute text-2xl text-zinc-600 cursor-pointer font-bold top-0.5 right-2">
+                        <i class="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
+                <div class="flex flex-col gap-4 px-8 pt-4 pb-8">
+                    <div>
+                        <label for="email" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Email</label>
+                        <input name="email" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                    </div>
+
+                    <div>
+                        <label for="password" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Password</label>
+                        <input name="password" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                    </div>
+
+                    <button class="block bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-gray-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Log in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
