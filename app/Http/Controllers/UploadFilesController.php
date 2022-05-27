598d743b9068a0ac621c2fef17db04df3530d5a9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-27 22:54:34 +0700

    created method handler for upload files register

--- app/Http/Controllers/UploadFilesController.php
+++ app/Http/Controllers/UploadFilesController.php
@@ -129,4 +129,108 @@ class UploadFilesController extends Controller
             'message' => 'File uploaded successfully'
         ]);
     }
+    public function uploadKIP(Request $request)
+    {
+        $files = $request->file('kipFile');
+        $noRegister = $request->session()->get('noreg');
+        $newName = $noRegister . '-kip.' . $files->extension();
+
+        if (UploadFiles::where('noreg_ppdb', $noRegister)->first()) {
+            $dataFiles = [
+                'kip' => $newName,
+            ];
+            UploadFiles::where('noreg_ppdb', $noRegister)->update($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        } else {
+            $dataFiles = [
+                'noreg_ppdb' => $noRegister,
+                'kip' => $newName,
+            ];
+            UploadFiles::create($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'File uploaded successfully'
+        ]);
+    }
+    public function uploadKIS(Request $request)
+    {
+        $files = $request->file('kisFile');
+        $noRegister = $request->session()->get('noreg');
+        $newName = $noRegister . '-kis.' . $files->extension();
+
+        if (UploadFiles::where('noreg_ppdb', $noRegister)->first()) {
+            $dataFiles = [
+                'kis' => $newName,
+            ];
+            UploadFiles::where('noreg_ppdb', $noRegister)->update($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        } else {
+            $dataFiles = [
+                'noreg_ppdb' => $noRegister,
+                'kis' => $newName,
+            ];
+            UploadFiles::create($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'File uploaded successfully'
+        ]);
+    }
+    public function uploadKKS(Request $request)
+    {
+        $files = $request->file('kksFile');
+        $noRegister = $request->session()->get('noreg');
+        $newName = $noRegister . '-kks.' . $files->extension();
+
+        if (UploadFiles::where('noreg_ppdb', $noRegister)->first()) {
+            $dataFiles = [
+                'kks' => $newName,
+            ];
+            UploadFiles::where('noreg_ppdb', $noRegister)->update($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        } else {
+            $dataFiles = [
+                'noreg_ppdb' => $noRegister,
+                'kks' => $newName,
+            ];
+            UploadFiles::create($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'File uploaded successfully'
+        ]);
+    }
+    public function uploadPKH(Request $request)
+    {
+        $files = $request->file('pkhFile');
+        $noRegister = $request->session()->get('noreg');
+        $newName = $noRegister . '-pkh.' . $files->extension();
+
+        if (UploadFiles::where('noreg_ppdb', $noRegister)->first()) {
+            $dataFiles = [
+                'pkh' => $newName,
+            ];
+            UploadFiles::where('noreg_ppdb', $noRegister)->update($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        } else {
+            $dataFiles = [
+                'noreg_ppdb' => $noRegister,
+                'pkh' => $newName,
+            ];
+            UploadFiles::create($dataFiles);
+            $files->move(public_path('assets/img/' . $noRegister . '/'), $newName);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'File uploaded successfully'
+        ]);
+    }
 }
