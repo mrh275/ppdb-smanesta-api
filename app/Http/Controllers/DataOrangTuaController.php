d5f93ff3fd85343856e7df50a59bceb02644c417
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-27 11:33:29 +0700

    added request validation when received request from data orang tua form

--- app/Http/Controllers/DataOrangTuaController.php
+++ app/Http/Controllers/DataOrangTuaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataOrangTua;
 use Illuminate\Http\Request;
 
 class DataOrangTuaController extends Controller
@@ -34,7 +35,66 @@ class DataOrangTuaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'nama_ayah' => 'required|min:3',
+            'tempat_lahir_ayah' => 'required|min:3',
+            'tanggal_lahir_ayah' => 'required',
+            'pendidikan_ayah' => 'required',
+            'pekerjaan_ayah' => 'required',
+            'penghasilan_ayah' => 'required',
+            'alamat_ayah' => 'required|min:15',
+            'nama_ibu' => 'required|min:3',
+            'tempat_lahir_ibu' => 'required|min:3',
+            'tanggal_lahir_ibu' => 'required',
+            'pendidikan_ibu' => 'required',
+            'pekerjaan_ibu' => 'required',
+            'penghasilan_ibu' => 'required',
+            'alamat_ibu' => 'required|min:15',
+        ];
+
+        $errorMessage = [
+            'nama_ayah.required' => 'Nama Ayah tidak boleh kosong',
+            'nama_ayah.min' => 'Nama Ayah minimal 3 karakter',
+            'tempat_lahir_ayah.required' => 'Tempat Lahir Ayah tidak boleh kosong',
+            'tempat_lahir_ayah.min' => 'Tempat Lahir Ayah minimal 3 karakter',
+            'tanggal_lahir_ayah.required' => 'Tanggal Lahir Ayah tidak boleh kosong',
+            'pendidikan_ayah.required' => 'Pendidikan Ayah tidak boleh kosong',
+            'pekerjaan_ayah.required' => 'Pekerjaan Ayah tidak boleh kosong',
+            'penghasilan_ayah.required' => 'Penghasilan Ayah tidak boleh kosong',
+            'alamat_ayah.required' => 'Alamat Ayah tidak boleh kosong',
+            'alamat_ayah.min' => 'Alamat Ayah minimal 15 karakter',
+            'nama_ibu.required' => 'Nama Ibu tidak boleh kosong',
+            'nama_ibu.min' => 'Nama Ibu minimal 3 karakter',
+            'tempat_lahir_ibu.required' => 'Tempat Lahir Ibu tidak boleh kosong',
+            'tempat_lahir_ibu.min' => 'Tempat Lahir Ibu minimal 3 karakter',
+            'tanggal_lahir_ibu.required' => 'Tanggal Lahir Ibu tidak boleh kosong',
+            'pendidikan_ibu.required' => 'Pendidikan Ibu tidak boleh kosong',
+            'pekerjaan_ibu.required' => 'Pekerjaan Ibu tidak boleh kosong',
+            'penghasilan_ibu.required' => 'Penghasilan Ibu tidak boleh kosong',
+            'alamat_ibu.required' => 'Alamat Ibu tidak boleh kosong',
+            'alamat_ibu.min' => 'Alamat Ibu minimal 15 karakter',
+        ];
+
+        $validatedDataOrangTua = $request->validate($rules, $errorMessage);
+        $tgl_lahir = str_replace('/', '-', $request->tanggal_lahir);
+        $validatedDataOrangTua['tanggal_lahir_ayah'] = date('Y-m-d', strtotime($tgl_lahir));
+        $validatedDataOrangTua['tanggal_lahir_ibu'] = date('Y-m-d', strtotime($tgl_lahir));
+
+        if ($request->session()->exists('noreg')) {
+            $validatedDataOrangTua['noreg_ppdb'] = $request->session()->get('noreg');
+            DataOrangTua::create($validatedDataOrangTua);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data berhasil disimpan',
+            ]);
+        } else {
+            return response()->json([
+                'success' => 'warning',
+                'message' => 'Nomor Registrasi tidak ditemukan.',
+                'text'    => 'Silahkan lakukan pendaftaran ulang. Dan meminta untuk reset data terlebih dahulu kepada panitia.',
+            ]);
+        }
     }
 
     /**
