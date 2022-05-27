51ac307075877f77227a00dd9ee0b756c81f18e2
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-27 11:35:42 +0700

    added some properties

--- resources/views/layouts/partials/components/access-denied.blade.php
+++ resources/views/layouts/partials/components/access-denied.blade.php
@@ -1,6 +1,6 @@
 <div class="py-6 sm:py-8 lg:py-12" id="close-access-denied">
     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-        <div class="md:h-80 flex flex-col sm:flex-row bg-gray-200 rounded-lg overflow-hidden relative">
+        <div class="flex flex-col sm:flex-row bg-gray-200 rounded-lg overflow-hidden relative">
             <button type="button" class="close-popup-denied-btn absolute top-0 right-2 text-2xl text-zinc-400 hover:text-zinc-600">
                 <i class="fa-solid fa-xmark"></i>
             </button>
@@ -11,14 +11,19 @@
             <!-- image - end -->
 
             <!-- content - start -->
-            <div class="w-full sm:w-1/2 lg:w-3/5 flex flex-col p-4 sm:p-8">
+            <div class="w-full h-max sm:w-1/2 lg:w-3/5 flex flex-col p-4 sm:p-8">
                 <h2 class="text-gray-800 text-xl md:text-2xl lg:text-4xl font-bold mb-4">Eits... Gak bisa wlee!😛</h2>
 
                 <p class="max-w-md text-gray-600 mb-1">
-                    Untuk melakukan pendaftaran silahkan datang langsung ke sekolah kami.
+                    Untuk melakukan pendaftaran silahkan hubungi panitia atau datang langsung ke sekolah kami.
                 </p>
-                <p class="max-w-md text-gray-600 mb-4">
+                <p class="max-w-md text-gray-600 mb-2">
                     Kalo temen-temen gak tau lokasi sekolah kami, bisa klik tombol dibawah ini.
+                </p>
+                <p class="narahubung flex flex-col mb-6">
+                    <a href="https://wa.me/6285155288214?text=Permisi,%20saya%20tulis%20nama%20anda%20dari%20tulis%20nama%20sekolah%20anda.%20Saya%20ingin%20mendaftar%20ke%20SMAN%201%20Rawamerta" class="text-zinc-700 hover:text-[#0099ff]"><i class="fa-brands fa-whatsapp"></i> Muhamad Ramdani Hidayatullah</a>
+                    <a href="https://wa.me/6285710122557?text=Permisi,%20saya%20tulis%20nama%20anda%20dari%20tulis%20nama%20sekolah%20anda.%20Saya%20ingin%20mendaftar%20ke%20SMAN%201%20Rawamerta" class="text-zinc-700 hover:text-[#0099ff]"><i class="fa-brands fa-whatsapp"></i> Wardiyanto, S.Pd.</a>
+                    <a href="https://wa.me/6285716339816?text=Permisi,%20saya%20tulis%20nama%20anda%20dari%20tulis%20nama%20sekolah%20anda.%20Saya%20ingin%20mendaftar%20ke%20SMAN%201%20Rawamerta" class="text-zinc-700 hover:text-[#0099ff]"><i class="fa-brands fa-whatsapp"></i> Yuliansyah Dinata, M.Pd.</a>
                 </p>
 
                 <div class="mt-auto">
