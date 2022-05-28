b5aba9a2cedb45ac171b151072ef3a283ba828d7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-29 01:59:58 +0700

    fixes some class on datatable

--- resources/views/data-pendaftar.blade.php
+++ resources/views/data-pendaftar.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.template')
 
 @section('content')
-    <div class="wrapper pt-20 lg:pt-32 px-10 pb-10 bg-zinc-200 dark:bg-zinc-800">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <tbody class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 text-center">
+    <div class="wrapper pt-20 lg:pt-32 px-10 pb-10 bg-zinc-200">
+        <div class="relative shadow-md rounded-xl bg-white px-6 py-8">
+            <h2 class="data-pendaftar-title">Data Pendaftar</h2>
+            @php
+                $jalur = ['KETM', 'Disabilitas', 'Kondisi Tertentu', 'Prestasi Rapor', 'Prestasi Kejuaraan', 'Perpindahan Orang Tua/Anak Guru', 'Zonasi'];
+            @endphp
+            <table id="data-pendaftar" class="display w-full text-sm text-left text-gray-700">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50" id="table-header">
+                    <tr class="border-b bg-zinc-100" id="row-header ">
                         <th scope="col" class="px-2 w-2 py-5">
                             No
                         </th>
@@ -19,12 +23,17 @@
                             NAMA LENGKAP
                         </th>
                         <th scope="col" class="px-2 py-5">
-                            Asal Sekolah
+                            JALUR PENDAFTARAN
+                        </th>
+                        <th scope="col" class="px-2 py-5">
+                            ASAL SEKOLAH
                         </th>
                     </tr>
+                </thead>
+                <tbody>
                     @foreach ($biodata as $item)
-                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
-                            <th scope="row" class="px-5 py-3 font-medium text-gray-900 dark:text-white whitespace-nowrap text-center">
+                        <tr class="border-b even:bg-white odd:bg-gray-100 ">
+                            <th scope="row" class="px-5 py-3 font-medium text-gray-700 whitespace-nowrap text-center">
                                 {{ $loop->iteration }}
                             </th>
                             <td class="px-5 py-3 text-center">
@@ -35,6 +44,9 @@
                             </td>
                             <td class="px-5 py-3">
                                 {{ $item->nama }}
+                            </td>
+                            <td class="px-5 py-3 text-center">
+                                {{ $jalur[$item->jalur_pendaftaran - 1] }}
                             </td>
                             <td class="px-5 py-3 text-center">
                                 {{ $item->asal_sekolah }}
@@ -51,7 +63,19 @@
 @push('scripts')
     <script>
         $(document).ready(function() {
-            $('#data-pendaftar').DataTable();
+            $('#data-pendaftar').DataTable({
+                scrollX: true,
+                autoWidth: false,
+                lengthChange: false,
+                responsive: false,
+                "initComplete": function(settings, json) {
+                    $('#data-pendaftar thead th').css('text-align', 'center');
+                    $('#data-pendaftar thead th').css('padding', '1.25rem 0.5rem');
+                    $('#data-pendaftar thead th').css('font-size', '0.9rem');
+                }
+            });
+
+            $('#myTable').DataTable().columns.adjust()
         });
     </script>
 @endpush
