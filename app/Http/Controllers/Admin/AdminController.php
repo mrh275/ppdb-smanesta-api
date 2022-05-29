73f4847ae22d8b5c1411831e7bdcfc844eb22e79
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-29 20:34:19 +0700

    created admin controller

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function dashboard(Request $request)
+    {
+        $data = [
+            'title' => 'Dashboard',
+        ];
+
+        return view('admin.dashboard', compact('data'));
+    }
+}
