deb2dc55e06ace99457c26e27a1ccb5197042adf
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-29 19:11:22 +0700

    separated and filtered library source by view page

---
+++ public/assets/js/main.js
@@ -0,0 +1,112 @@
+// Navbar Animation on Scroll
+let navbar = document.querySelector(".navbar");
+let offset = 0;
+window.addEventListener("scroll", function () {
+    let st = window.pageYOffset;
+    if (st > offset) {
+        navbar.classList.add("fixed-nav");
+        // document.querySelector(".scroll-top-btn").classList.add("bottom-20");
+        // document.querySelector(".scroll-top-btn").classList.remove("-bottom-10");
+    } else {
+        navbar.classList.remove("fixed-nav");
+        // document.querySelector(".scroll-top-btn").classList.remove("bottom-20");
+        // document.querySelector(".scroll-top-btn").classList.add("-bottom-10");
+    }
+});
+
+// Progress Bar fixed on scroll
+let progressBar = document.querySelector(".progress-bar");
+let offsetProgressBar = 91;
+if (window.location.pathname == "/register") {
+    window.addEventListener("scroll", function () {
+        let st = window.pageYOffset;
+        if (st > offsetProgressBar) {
+            progressBar.classList.add("fixed-nav");
+        } else {
+            progressBar.classList.remove("fixed-nav");
+        }
+    });
+}
+// Toggle Menu
+let hMenu = document.querySelector(".toggle-menu");
+hMenu.addEventListener("click", function () {
+    if (!hMenu.classList.contains("show")) {
+        this.classList.add("show");
+        document.querySelector(".nav-menu").classList.add("show");
+        document.querySelector(".navbar").classList.add("show");
+    } else {
+        this.classList.remove("show");
+        document.querySelector(".nav-menu").classList.remove("show");
+        document.querySelector(".navbar").classList.remove("show");
+    }
+});
+
+// Toggle dark mode
+const toggleSwitch = document.querySelector("#toggle-darkMode");
+toggleSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+// Fasilitas Carousel
+if (window.innerWidth < 768) {
+    // Carousel
+    document.addEventListener("DOMContentLoaded", function () {
+        var splide = new Splide(".splide", {
+            type: "loop",
+            perPage: 2,
+            perMove: 2,
+            arrows: false,
+            classes: {
+                pagination: "splide__pagination fasilitas-pagination",
+                page: "splide__pagination__page fasilitas-page",
+            },
+        });
+        splide.mount();
+    });
+} else {
+    // Carousel
+    document.addEventListener("DOMContentLoaded", function () {
+        var splide = new Splide(".splide", {
+            type: "loop",
+            perPage: 3,
+            perMove: 3,
+            arrows: false,
+            classes: {
+                pagination: "splide__pagination fasilitas-pagination",
+                page: "splide__pagination__page fasilitas-page",
+            },
+        });
+        splide.mount();
+    });
+}
+// Show Login Form
+const lastMenu = document.querySelector(".nav-menu").lastElementChild;
+
+if (lastMenu.querySelector("a").innerHTML != "Logout")
+    document
+        .querySelector("#login-btn")
+        .addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector("#login-form").classList.remove("invisible");
+            document.querySelector("#login-form").classList.remove("opacity-0");
+        });
+
+// Close Login Form
+document
+    .querySelector("#close-login-btn")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector("#login-form").classList.add("invisible");
+        document.querySelector("#login-form").classList.add("opacity-0");
+    });
+
+// Show Password
+document
+    .querySelector("#showPassword")
+    .addEventListener("change", function (e) {
+        if (this.checked) {
+            document.querySelector("#password").type = "text";
+        } else {
+            document.querySelector("#password").type = "password";
+        }
+    });
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,25 +11,29 @@
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/png">
 
     {{-- CSS --}}
-    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.2/dist/css/splide.min.css">
-
-    {{-- Font --}}
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.2/dist/css/splide.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    @if (Request::segment(1) == 'register')
+        <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.css" />
+    @endif
 
     {{-- Library --}}
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.14/sweetalert2.min.css" />
-    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-    <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+    @if (Request::segment(1) == 'register')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.14/sweetalert2.min.css" />
+        <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+    @endif
+    @if (Request::segment(1) == 'data-pendaftar')
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+        <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
+        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    @endif
 
 </head>
 
@@ -43,9 +47,11 @@
 
     {{-- JS --}}
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.2/dist/js/splide.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.14/sweetalert2.min.js"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
+    @if (Request::segment(1) == 'register')
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.14/sweetalert2.min.js"></script>
+    @endif
+    <script src="{{ asset('assets/js/main.js') }}"></script>
     @stack('scripts')
     @stack('specifyjs')
     <script>
