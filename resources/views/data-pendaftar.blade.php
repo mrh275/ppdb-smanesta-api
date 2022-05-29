01eafb76a4c7599cc47a5fa47e2d86be5f24f29b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-29 16:18:06 +0700

    fixes datatable layout and properties

--- resources/views/data-pendaftar.blade.php
+++ resources/views/data-pendaftar.blade.php
@@ -1,59 +1,48 @@
 @extends('layouts.template')
 
 @section('content')
-    <div class="wrapper pt-20 lg:pt-32 px-10 pb-10 bg-zinc-200">
+    <div class="wrapper data-pendaftar-wrapper pt-20 lg:pt-32 px-4 lg:px-10 pb-10 bg-zinc-200">
         <div class="relative shadow-md rounded-xl bg-white px-6 py-8">
             <h2 class="data-pendaftar-title">Data Pendaftar</h2>
             @php
                 $jalur = ['KETM', 'Disabilitas', 'Kondisi Tertentu', 'Prestasi Rapor', 'Prestasi Kejuaraan', 'Perpindahan Orang Tua/Anak Guru', 'Zonasi'];
             @endphp
-            <table id="data-pendaftar" class="display w-full text-sm text-left text-gray-700">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50" id="table-header">
-                    <tr class="border-b bg-zinc-100" id="row-header ">
-                        <th scope="col" class="px-2 w-2 py-5">
-                            No
-                        </th>
-                        <th scope="col" class="px-2 py-5">
-                            Nomor Pendaftaran
-                        </th>
-                        <th scope="col" class="px-2 py-5">
-                            NISN
-                        </th>
-                        <th scope="col" class="px-2 py-5">
-                            NAMA LENGKAP
-                        </th>
-                        <th scope="col" class="px-2 py-5">
-                            JALUR PENDAFTARAN
-                        </th>
-                        <th scope="col" class="px-2 py-5">
-                            ASAL SEKOLAH
-                        </th>
+            <table id="data-pendaftar" class="stripe" style="width:100%;">
+                <thead>
+                    <tr>
+                        <th data-priority="1">No</th>
+                        <th data-priority="2">No. Pendaftaran</th>
+                        <th data-priority="3">NISN</th>
+                        <th data-priority="4">Nama Lengkap</th>
+                        <th data-priority="5">Jalur Pendaftaran</th>
+                        <th data-priority="6">Asal Sekolah</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($biodata as $item)
-                        <tr class="border-b even:bg-white odd:bg-gray-100 ">
-                            <th scope="row" class="px-5 py-3 font-medium text-gray-700 whitespace-nowrap text-center">
+                        <tr role="row">
+                            <td class="text-center">
                                 {{ $loop->iteration }}
-                            </th>
-                            <td class="px-5 py-3 text-center">
+                            </td>
+                            <td>
                                 {{ $item->noreg_ppdb }}
                             </td>
-                            <td class="px-5 py-3 text-center">
+                            <td>
                                 {{ $item->nisn }}
                             </td>
-                            <td class="px-5 py-3">
+                            <td>
                                 {{ $item->nama }}
                             </td>
-                            <td class="px-5 py-3 text-center">
+                            <td>
                                 {{ $jalur[$item->jalur_pendaftaran - 1] }}
                             </td>
-                            <td class="px-5 py-3 text-center">
+                            <td>
                                 {{ $item->asal_sekolah }}
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
+
             </table>
         </div>
 
@@ -64,18 +53,10 @@
     <script>
         $(document).ready(function() {
             $('#data-pendaftar').DataTable({
-                scrollX: true,
-                autoWidth: false,
-                lengthChange: false,
-                responsive: false,
-                "initComplete": function(settings, json) {
-                    $('#data-pendaftar thead th').css('text-align', 'center');
-                    $('#data-pendaftar thead th').css('padding', '1.25rem 0.5rem');
-                    $('#data-pendaftar thead th').css('font-size', '0.9rem');
-                }
-            });
-
-            $('#myTable').DataTable().columns.adjust()
+                    autoWidth: false,
+                    responsive: true,
+                }).columns.adjust()
+                .responsive.recalc();
         });
     </script>
 @endpush
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -24,7 +24,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.14/sweetalert2.min.css" />
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
