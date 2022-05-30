5ae64be124d94d76c83281b25d929317532da37a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-30 12:26:30 +0700

    modified footer

--- resources/views/admin/layouts/footer-admin.blade.php
+++ resources/views/admin/layouts/footer-admin.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021
-        <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; {{ date('Y') }}
+        <a href="https://sman1rawamerta.sch.id">SMAN 1 Rawamerta</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+        Made with ❤ by <a href="http://muhamadramdani275.github.io" target="_blank">dNezast</a>
     </div>
 </footer>
