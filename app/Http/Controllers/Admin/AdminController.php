3869950c0b0a72d1287787baf801869ac8b267c1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-05-30 12:22:12 +0700

    created controller for data pendaftar, seleksi pendaftar and hasil seleksi

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Biodata;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
@@ -14,5 +15,35 @@ class AdminController extends Controller
         ];
 
         return view('admin.dashboard', $data);
+    }
+
+    public function dataPendaftar(Request $request)
+    {
+        $data = [
+            'title' => 'Data Pendaftar',
+            'biodata' => Biodata::all()
+        ];
+
+        return view('admin.data-pendaftar', $data);
+    }
+
+    public function seleksiPendaftar(Request $request)
+    {
+        $data = [
+            'title' => 'Seleksi Pendaftar',
+            'biodata' => Biodata::all()
+        ];
+
+        return view('admin.seleksi-pendaftar', $data);
+    }
+
+    public function hasilSeleksi(Request $request)
+    {
+        $data = [
+            'title' => 'Hasil Seleksi',
+            'biodata' => Biodata::all()
+        ];
+
+        return view('admin.hasil-seleksi', $data);
     }
 }
