790749b25962563605c461c3e6a2c00c6d453980
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-09 17:02:23 +0700

    modified middleware name space for checking role

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'operator' => \App\Http\Middleware\Operator::class,
+        'checkRole' => \App\Http\Middleware\CheckRole::class,
     ];
 }
