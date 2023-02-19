c67f705687f87e314b44ccf60ef173d33214e451
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-19 13:19:53 +0700

    added api route for logout

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\LoginController;
 */
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
