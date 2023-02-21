9f8e2417c2d191ace3aef418660283431e638955
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-21 22:02:29 +0700

    create method to get data relation [data orang tua] using eloquent

--- app/Models/Biodata.php
+++ app/Models/Biodata.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\DataOrangTua;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Biodata extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function dataOrangTua()
+    {
+        return $this->hasMany(DataOrangTua::class, 'noreg_ppdb', 'noreg_ppdb');
+    }
 }
