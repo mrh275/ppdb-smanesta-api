739bb1c57026f00ed0d2822f38b5d994171bbf87
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-10-10 09:34:51 +0700

    modified auth route using new auth controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\BiodataController;
 use App\Http\Controllers\DataOrangTuaController;
 
@@ -17,10 +17,10 @@ use App\Http\Controllers\DataOrangTuaController;
 |
 */
 
-Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/data-pendaftar', [BiodataController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/active-pendaftar', [BiodataController::class, 'edit']);
     Route::post('/biodata', [BiodataController::class, 'store']);
     Route::post('/data-orangtua', [DataOrangTuaController::class, 'store']);
