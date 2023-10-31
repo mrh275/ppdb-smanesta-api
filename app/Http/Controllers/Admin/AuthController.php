0009a9942d04ef252edef1bd1b398e7c3438a56f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-10-31 23:18:11 +0700

    craete auth controller and login method for admin panel

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        try {
+            Auth::attempt($credentials);
+            $user = User::where('username', $credentials['username'])->first();
+            $token = $user->createToken('accessToken')->plainTextToken;
+            return response()->json([
+                'status' => 200,
+                'message' => 'Login successfully!',
+                'accessToken' => $token
+            ]);
+        } catch (Exception $error) {
+            return response()->json([
+                'status' => $error->getCode(),
+                'message' => $error->getMessage()
+            ]);
+        }
+    }
+}
