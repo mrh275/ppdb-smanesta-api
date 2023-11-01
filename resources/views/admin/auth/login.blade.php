0ee2e7d4659dbeda13070d6c32e4004a4dc9b271
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-11-01 21:11:10 +0700

    create login card

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,5 +1,15 @@
 @extends('admin.layouts.auth.layout-login')
 
 @section('content')
-    <h1 class="text-3xl font-bold text-indigo-500">Ini halaman login admin</h1>
+    <div class="flex items-center justify-center w-full h-screen login-wrapper">
+        <div class="w-1/4 px-4 py-4 mx-auto bg-gray-200 rounded-lg shadow-sm card-login">
+            <div class="flex flex-col items-center card-header">
+                <img src="{{ asset('assets/img/LOGO-SMALL.png') }}" alt="Logo Sekolah" class="mb-2 w-14 card-img">
+                <h3 class="text-3xl font-bold text-gray-700 card-title">Login Admin</h3>
+                <h4 class="text-xl font-bold text-gray-700 card-subtitle">PPDB SEMESTA</h4>
+            </div>
+            <div class="card-body">
+            </div>
+        </div>
+    </div>
 @endsection
