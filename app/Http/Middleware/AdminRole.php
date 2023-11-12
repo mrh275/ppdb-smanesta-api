05ba051c35beb1ec103606dafff2d49915a5c313
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-11-12 16:10:28 +0700

    created admin role middleware

---
+++ app/Http/Middleware/AdminRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+
+class AdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            if ($request->user()->username != null) {
+                if ($request->user()->username == 'administrator') {
+                    return $next($request);
+                } else {
+                    return redirect()->route('adminDashboard');
+                }
+            } else {
+                return redirect()->route('login');
+            }
+        } catch (Exception $error) {
+            return redirect()->route('login');
+        }
+    }
+}
