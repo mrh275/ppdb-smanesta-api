1e48d55cb0285e5d484aea7bbfc20873458fe4e8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-11-13 17:02:26 +0700

    update assets module render

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19442,6 +19442,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/css/edit-registrant.css":
+/*!*******************************************!*\
+  !*** ./resources/css/edit-registrant.css ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./node_modules/process/browser.js":
 /*!*****************************************!*\
   !*** ./node_modules/process/browser.js ***!
@@ -19748,6 +19761,7 @@ process.umask = function() { return 0; };
 /******/ 		var installedChunks = {
 /******/ 			"/assets/js/app": 0,
 /******/ 			"assets/css/app": 0,
+/******/ 			"assets/css/edit-registrant": 0,
 /******/ 			"assets/css/admin-login": 0
 /******/ 		};
 /******/ 		
@@ -19798,9 +19812,10 @@ process.umask = function() { return 0; };
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["assets/css/app","assets/css/admin-login"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["assets/css/app","assets/css/admin-login"], () => (__webpack_require__("./resources/css/app.css")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["assets/css/app","assets/css/admin-login"], () => (__webpack_require__("./resources/css/admin-login.css")))
+/******/ 	__webpack_require__.O(undefined, ["assets/css/app","assets/css/edit-registrant","assets/css/admin-login"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["assets/css/app","assets/css/edit-registrant","assets/css/admin-login"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["assets/css/app","assets/css/edit-registrant","assets/css/admin-login"], () => (__webpack_require__("./resources/css/admin-login.css")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["assets/css/app","assets/css/edit-registrant","assets/css/admin-login"], () => (__webpack_require__("./resources/css/edit-registrant.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
